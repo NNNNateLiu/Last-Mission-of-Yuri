7b6c1c584473478333514675600e3e8a56b26bf9
NateLiu <sl8292@nyu.edu>
2021-04-03 00:29:51 +0800

    Radio Fully Functionlized

--- USSR/Assets/Scripts/Radios/NumberController.cs
+++ USSR/Assets/Scripts/Radios/NumberController.cs
@@ -7,17 +7,21 @@ public class NumberController : MonoBehaviour
 {
     public GameObject radio;
     public string shownNumber;
-    public int number;
+    public int numberOrder;
     public Text mtext;
 
     // Update is called once per frame
     void Update()
     {
-        mtext.text = shownNumber;
-    }
+        mtext.text = RadioController.instance.numbers[numberOrder].ToString();
 
-    public void SwitchNumber()
-    {
-
+        if(numberOrder == RadioController.instance.currentContrillingNumber)
+        {
+            mtext.color = Color.red;
+        }
+        else
+        {
+            mtext.color = Color.black;
+        }
     }
 }
