b7ca5e0f9f4c045ad08d383d152d34fd73b82fc7
Yangying <ryysophie@126.com>
2021-04-23 23:31:37 +0800

    Auto stash before merge of "main" and "origin/main"+add animation for post processing

---
+++ USSR/Assets/Scripts/PPAdjust.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPAdjust : MonoBehaviour
+{ public GameObject DoorTrigger;
+  public GameObject SwitchPP;
+  public GameObject OldPP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DoorTrigger.GetComponent<Level1RedGreenDoor>().stopToOpen){
+            SwitchPP.SetActive(true);
+            OldPP.SetActive(false);
+
+          }
+        
+    }
+}
