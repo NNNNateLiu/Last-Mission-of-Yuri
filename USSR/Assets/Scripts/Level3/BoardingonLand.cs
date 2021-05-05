e52a75366d16d4ef45688788758199dfa1269f3a
NateLiu <sl8292@nyu.edu>
2021-05-05 11:48:48 +0800

    seprate level3 into Models scene and LD scenes

---
+++ USSR/Assets/Scripts/Level3/BoardingonLand.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardingonLand : MonoBehaviour
+{
+    public GameObject PlayerPosition;
+    public int boardingtime;
+    public bool isActive;
+    public AudioSource walkSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.timeSinceLevelLoad>boardingtime){
+            PlayerPosition.GetComponent<Animator>().enabled=false;
+        }
+        if(isActive==true){
+            if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.D)){
+                walkSound.Play();
+            }
+            else
+            {
+                walkSound.Pause();
+            }
+        }
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log(other.tag);
+        if (other.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log(other.tag);
+        if (other.tag=="Player")
+        {
+            isActive=false;
+        }
+    }
+}
