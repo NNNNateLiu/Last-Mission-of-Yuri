d7e5230747ee7aa65e29d86abde16fc9869b941c
NateLiu <sl8292@nyu.edu>
2021-05-05 20:02:33 +0800

    some changes for final versions

--- USSR/Assets/Scripts/Level3/Level3StageTrigger.cs
+++ USSR/Assets/Scripts/Level3/Level3StageTrigger.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Level3StageTrigger : StageTrigger
 {
     public override void OnInteract()
     {
-        doorCollider.SetActive(false);
+        doorCollider.transform.DOLocalMoveZ(-8f, 3f);
     }
 }
